4bc101ebf17aa7e5e804c2f5d8721d4c59639d4f
Šimon Brauner <simonbrauner@seznam.cz>
2025-11-01 19:57:02 +0100

    feat: install button

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,8 @@
 self.addEventListener('install', (event) => {
+    console.log('[Service Worker] Installed successfully.');
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    self.clients.claim()
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
+    console.log('[Service Worker] Activated.');
 });
